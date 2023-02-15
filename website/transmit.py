5dd3ad60c953bb8a398dfb973abd0b3bacbbf65f
Petter Pfalzer <pfalzerpetter@gmail.com>
2023-02-15 22:43:35 +0000

    Did a huge optimization on the radio modules by comressing data, need to add compressing and dekompressing of the data also on the cansat side

--- website/transmit.py
+++ website/transmit.py
@@ -8,32 +8,103 @@ from .models import GPSdata, Telemdata
 
 from sys import getsizeof
 
-
+import threading
 import logging
-import sqlite3
 import random
 import time 
 import json 
 
 
 
+
+
+
+
+
+
+
+
+
+"""
+________________________________________ uncompressData _________________________________________
+
+This function uncompresses the data from minimal charachters to a full sized dict, there is also a function to comress the data.
+
+Forexmaple if you want to uncompress the tring: 
+34.1,34.1,23.5,-0.0017278495943173766,12649492919445038,9652832746505737,
+
+Then this script will output:
+{"temprature": 34.1, "tempPressure": 34.1, "humidity": 23.5, "acceleration": {"x": -0.0017278495943173766, "y": 0.12649492919445038, "z": 0.9652832746505737}}
+
+dictValues = This is the values that the empty dictonary will hold, aka the compressed string  
+emptyDict = This is a empty dictorary that wil be filled in of the inputted compressed data NOTE you need to have the same LENGHT BETWEEN THE "dictValues" and the "emptyDict"
+seperator = This is the seperator you want to have between the data
+
 """
 
-Send data Dict is 232 bytes
+emptyDict = {"temprature": None, "tempPressure": None, "humidity": None, "acceleration": {"x": None, "y": None, "z": None}}
+
+def uncompressData(dictValues, emptyDict, seperator=","):
+    dictValues = dictValues.split(seperator)
+    emptyDict = {"temprature": None, "tempPressure": None, "humidity": None, "acceleration": {"x": None, "y": None, "z": None}}
+    emptyDict_ = emptyDict # SO THE SCIPT DOSENT OVERWRITE THE EMPTY DICTIONARY
+
+
+    if len(dictValues) == len(emptyDict_) or True: # IF THE INPUT DATA IS VALID LENGTH
+        for i, key in enumerate(emptyDict_.keys()): # LOOPS OVER ALL OF THE KEYS IN THE DICTIONARY, THE "i" VARIABLE KEEPS TRACK OVER WHAT VALUE OF THE LSIT WE ARE ON
+            if emptyDict_[key] != None: # IF THERE IS A NESTED DICTIONARY, FOR EXAMPLE "acceleration": {"x": None, "y": None, "z": None}"
+                for nestedKey in emptyDict_[key].keys(): # LOOPS OVER ALL OF THE KEYS IN THE PARENT KEY
+                    emptyDict_[key][nestedKey] = dictValues[i] # SETS THAT VALUE TO THE VALUE IN THE LIST
+                    i+=1 # ADDS ONE TO I, TO KEEP THE ORDER RIGHT ACCORDING TO THE "dictValues" LIST
+            else: 
+                emptyDict_[key] = dictValues[i] # SET THAT KEY TO THE CURRENT VALUE OF THE DICT VALUE
+        return emptyDict_
+    else: 
+        raise Exception("The empty dict and the dictvalues is not the same lenght, plase check the input data")
 
 
 """
+_________________________________________ compressData __________________________________________
+
+This function compresses a dict to the minimal characters, this is to improve the max frequency the rfm9x radios can communicate together, there is also a function to uncompress the data
+
+Forexmaple if you want to compress the dict: 
+{"temprature": 34.1, "tempPressure": 34.1, "humidity": 23.5, "acceleration": {"x": -0.0017278495943173766, "y": 0.12649492919445038, "z": 0.9652832746505737}}
+
+Then this script will output:
+34.1,34.1,23.5,-0.0017278495943173766,12649492919445038,9652832746505737,
+
+dictionary = This is the dictonary of data you want to compress (dict)
+seperator = This is the seperator you want to have between the data
+
+"""
+
+def compressData(dictionary, seperator=","): 
+    rtnString = "" # THIS KEEPS TRACK OF THE ALREADY COMPRESSED DATA
+
+    for values in dictionary.values(): # LOOPS OVER ALL OF THE VALUES IN THE INPUT DICTIONARY
+        if isinstance(values, dict): # IT THE TYPE OF THE VALUE IS A DICT, MEANING THAT THERE IS A NESTED DICT EXAMPLE "{"temprature": 34.1, "acceleration": {"x": -0.0017278495943173766, "y": 0.12649492919445038, "z": 0.9652832746505737}}"
+            for nestedValues in values.values(): # LOOP OVER ALL OF THE ITEMS IN THE VALUE'S DICT
+                rtnString += str(nestedValues) + seperator # ADD THE VALUE TO THE "rtnString" VARIABLE, AND ADD A SEPERATOR EXAMPLE ","
+            break
+
+        rtnString += str(values) + seperator # ADD THE VALUE TO THE "rtnString" VARIABLE, AND ADD A SEPERATOR EXAMPLE ","
+    return rtnString # RETURNS THE VARIABLE
+
+
+
+
 
 def sendData():
     with open(pathToTransmitJson, "r+") as inFile: # OPENS THE TRANSMIT FILE
         jsonData = json.load(inFile) # LOADS THE FILE
-        radio.send(bytes(str(jsonData), "utf-8")) # SENDS THE DATA WITH THE "rfm9x" LORA RADIO MODULE
-
+        compressedData = compressData(jsonData)
+        radio.send(bytes(compressedData, "utf-8")) # SENDS THE DATA WITH THE "rfm9x" LORA RADIO MODULE
 
     return jsonData
         
 
-def reciveDataFunc():
+def writeReciveData(recivedData):
 
     prevReciveData = {
         "gps": {
@@ -52,56 +123,38 @@ def reciveDataFunc():
             "temperature": 22.23}}
     
 
-    for i in range(10):
-        packet = radio.receive()
-        print(i)
 
-        if packet != None: 
-            break 
 
-    if packet is not None: 
-        try:
-            recvieData = str(packet, "utf-8")
-        except: 
-            logging.critical(f"     Error unpacking packet from cansat, packet recived: {packet}")
-            return None
-    else: 
-        logging.error(f"     Packet from cansat is: {packet}")
-        return None
-        
-    
 
-    if recvieData != prevReciveData and writeRecivedData == True: # IF THERE IS NEW DATA
 
 # -- GETS THE LAST FLIGHT ID AND THE START TIME OF THAT FLIGHT
-        flightData = selectFromDB(pathToDB, "flightmaster", ["WHERE"], ["loginId"], ["1"], log=False) # GETS ALL OF THE PREVIUS FLIGHTS OF THE ADMIN
-        latestFlightID = flightData[len(flightData) -1][0] # SELECTS THE LATEST FLIGHT ID (int)
+    flightData = selectFromDB(pathToDB, "flightmaster", ["WHERE"], ["loginId"], ["1"], log=False) # GETS ALL OF THE PREVIUS FLIGHTS OF THE ADMIN
+    latestFlightID = flightData[len(flightData) -1][0] # SELECTS THE LATEST FLIGHT ID (int)
 
-        latestFlightStartTime = flightData[len(flightData) -1][2] # FINDS THE LATEST FLIGHT STARTTIME, RETURNS YEAR:MONTH HR:MIN:SEC
-        latestFlightStartTime = latestFlightStartTime.split(" ")[1] # CONVERTS THE latestFlightStartTime TO HR:MIN:SEC
-        flightStartMin = timeToMinutes(latestFlightStartTime) # CONVERTS THE LATEST FLIGHTSTARTTIME TO MINUTES
+    latestFlightStartTime = flightData[len(flightData) -1][2] # FINDS THE LATEST FLIGHT STARTTIME, RETURNS YEAR:MONTH HR:MIN:SEC
+    latestFlightStartTime = latestFlightStartTime.split(" ")[1] # CONVERTS THE latestFlightStartTime TO HR:MIN:SEC
+    flightStartMin = timeToMinutes(latestFlightStartTime) # CONVERTS THE LATEST FLIGHTSTARTTIME TO MINUTES
 
 
 # -- CALCULATES THE ESTIMATED TIME SINCE THE CANSAT WAS STARTED
-        currentFlightMin = timeToMinutes("_", currentTime=True) # GETS THE CURRENT FLIGHT TIME IN MINUTES
-        estimatedFlightTime=currentFlightMin-flightStartMin # CALCULATES THE ESTMATED FLIGHT TIME
+    currentFlightMin = timeToMinutes("_", currentTime=True) # GETS THE CURRENT FLIGHT TIME IN MINUTES
+    estimatedFlightTime=currentFlightMin-flightStartMin # CALCULATES THE ESTMATED FLIGHT TIME
 
 # -- WRITES THE DATA RECIVED TO THE DB
-        gpsData = GPSdata(flightId=latestFlightID, lat=recivedData["gps"]["lat"], lon=recivedData["gps"]["lon"])
-        telemData = Telemdata(flightId=latestFlightID, atmoPressure=recivedData["telemdata"]["atmoPressure"], temperature=recivedData["telemdata"]["temperature"], flightTime=estimatedFlightTime)
+    gpsData = GPSdata(flightId=latestFlightID, lat=recivedData["gps"]["lat"], lon=recivedData["gps"]["lon"])
+    telemData = Telemdata(flightId=latestFlightID, atmoPressure=recivedData["telemdata"]["atmoPressure"], temperature=recivedData["telemdata"]["temperature"], flightTime=estimatedFlightTime)
 
-        db.session.add(gpsData)
-        db.session.add(telemData)
-        db.session.commit()
+    db.session.add(gpsData)
+    db.session.add(telemData)
+    db.session.commit()
 
 
 # -- WRITES THE GATHERED DATA TO THE RECIVE.JSON FILE (NOT USED)
-        with open(pathToReciveJson, "w") as file: # OPENS THE FILE IN WRITE MODE 
-            json.dump(recivedData, file) # DUMPS THE JSON TO THE JSON
+    #with open(pathToReciveJson, "w") as file: # OPENS THE FILE IN WRITE MODE 
+    #    json.dump(recivedData, file) # DUMPS THE JSON TO THE JSON
 
-        prevReciveData = recvieData  #NOTE NOTE NOTE NOTE NOTE NEEDS TO BE UNCOMMEDED TO WORK
 
-    return recvieData # RETURNS TRUE TO NOT STOP READING AND WRITING
+    return recivedData # RETURNS TRUE TO NOT STOP READING AND WRITING
 
 
 
@@ -109,6 +162,61 @@ def reciveDataFunc():
 
 def elapsedTime(startTimeFloat):
     return time.time() - startTimeFloat 
+
+def TX_RX_main(app): 
+    with app.app_context(): # TO GET FULL PERMISSIONS TO READ AND WRITE TO DB
+        while True: 
+            packet = radio.receive() # GETS DATA FROM THE RADIO
+
+            if packet is not None: # IF THERE IS A PACKET 
+                recvieData = uncompressData(dictValues = str(packet, "utf-8"), emptyDict=emptyDict) # TRANSFORMES THE DATA TO A READEBLE DICTIONARY
+                print(recvieData)
+                writeReciveData(recvieData) # WRITES THE DATA TO THE DB
+          
+                sendData() # SENDS THE DATA IN "transmit.json"
+
+
+
+
+
+
+
+"""
+def TX_RX_main(app): 
+    with app.app_context(): # TO GET FULL PERMISSIONS TO READ AND WRITE TO DB
+        totalTime, i = 0, 0
+
+        while True: 
+            startTime = time.time()
+
+            reciveThread = threading.Thread(target=reciveDataFunc)
+            reciveThread.start()
+
+            if elapsedTime(startTime) < TX_RX_sleep * 0.8:
+                sendThread = threading.Thread(target=sendData)
+                sendThread.start()
+            else: 
+                logging.warning(f"     Didnt send data to Cansat, because there wasnt enough time left to forfill the time constraint of {TX_RX_sleep}. Total elapsed time in this cycle: {elapsedTime(startTime)}")
+                
+
+
+
+            if elapsedTime(startTime) < TX_RX_sleep: # ADD A BIT OF PADDING, BECAUSE IT TAKES A BIT OF TIME TO CAUSE THE SLEE
+                try:    
+                    time.sleep(TX_RX_sleep - elapsedTime(startTime))
+                except: 
+                    time.sleep(TX_RX_sleep - (elapsedTime(startTime) - 0.08))
+
+
+            if loggingLevel <= 10: # IF YOU WANT TO LOG OUT THE RESULT
+                    i+=1 # ADDS 1 TO THE INDEX VARIABLE, THIS IS TO CALCULATE AVG TIME
+                    totalTime += elapsedTime(startTime) # ADDS THE ELAPSED TIME TO THE TOTALTIME VARIABLE
+                    if str(i)[len(str(i)) -1] == "8": # IF I ENDS WITH "1" (EVRY 10TH TIME)
+                        logging.debug(f"     Avrage Transmit and recive time: {totalTime/i}") # LOG OUT THE AVG TIME
+
+
+
+
 
 
 def TX_RX_main(app):
@@ -159,7 +267,7 @@ def TX_RX_main(app):
 
 
 
-
+"""
 
 
 
